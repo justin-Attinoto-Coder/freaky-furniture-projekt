d30471c71b3f8cc72c347ad6d9b29a973a04a706
Justin Lee Attinoto <justinattinoto@gmail.com>
2025-05-07 14:34:01 +0200

    refactor: update component imports and improve logging in product details and focus product information components

--- client/src/app/components/product/focus-product-details-image/focus-product-details-image.component.spec.ts
+++ client/src/app/components/product/focus-product-details-image/focus-product-details-image.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { FocusProductDetailsImageComponent } from './focus-product-details-image.component';
+import { ProductDetailsImageComponent } from './focus-product-details-image.component';
 
-describe('FocusProductDetailsImageComponent', () => {
-  let component: FocusProductDetailsImageComponent;
-  let fixture: ComponentFixture<FocusProductDetailsImageComponent>;
+describe('ProductDetailsImageComponent', () => {
+  let component: ProductDetailsImageComponent;
+  let fixture: ComponentFixture<ProductDetailsImageComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [FocusProductDetailsImageComponent]
+      imports: [ProductDetailsImageComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(FocusProductDetailsImageComponent);
+    fixture = TestBed.createComponent(ProductDetailsImageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
