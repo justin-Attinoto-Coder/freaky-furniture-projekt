b2ff43023d638157584e522a66eda71aa53278ae
Justin Lee Attinoto <justinattinoto@gmail.com>
2025-05-11 09:53:21 +0200

    feat: add product reviews component with routing and API integration; update related components and templates for review display

---
+++ client/src/app/components/product/product-reviews/product-reviews.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faStar, faStar as faRegStar } from '@fortawesome/free-solid-svg-icons';
+import { HttpErrorResponse } from '@angular/common/http';
+
+interface Review {
+  id: number;
+  productId: number;
+  rating: number;
+  reviewText: string;
+  reviewerName: string;
+}
+
+@Component({
+  selector: 'app-product-reviews',
+  standalone: true,
+  imports: [CommonModule, FontAwesomeModule],
+  templateUrl: './product-reviews.component.html',
+  styleUrls: ['./product-reviews.component.css']
+})
+export class ProductReviewsComponent implements OnInit {
+  productId: number | null = null;
+  reviews: Review[] = [];
+  faStar = faStar;
+  faRegStar = faRegStar;
+
+  constructor(private route: ActivatedRoute, private http: HttpClient) {}
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      const urlSlug = params.get('urlSlug');
+      if (urlSlug) {
+        // Fetch product by urlSlug to get productId
+        this.http.get<any>(`http://localhost:8000/api/furniture/${urlSlug}`).subscribe({
+          next: (product) => {
+            this.productId = product.id;
+            this.fetchReviews();
+          },
+          error: (error: HttpErrorResponse) => {
+            console.error('ProductReviews: Error fetching product:', error);
+            this.productId = null;
+            this.reviews = [];
+          }
+        });
+      }
+    });
+  }
+
+  fetchReviews() {
+    if (this.productId) {
+      this.http.get<Review[]>(`http://localhost:8000/api/reviews/${this.productId}`).subscribe({
+        next: reviews => {
+          this.reviews = reviews;
+          console.log('ProductReviews: Fetched reviews:', reviews);
+        },
+        error: (error: HttpErrorResponse) => {
+          console.error('ProductReviews: Error fetching reviews:', error);
+          this.reviews = [];
+        }
+      });
+    }
+  }
+}
