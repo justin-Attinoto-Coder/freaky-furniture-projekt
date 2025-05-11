e5720cca8c3977216df7b1d4b22e5f4a83b5fe87
Justin Lee Attinoto <justinattinoto@gmail.com>
2025-05-11 10:29:18 +0200

    feat: update admin routing and navigation; enhance admin sidebar and topbar components for improved user experience. organized admin and login

--- client/src/app/components/admin/admin-topbar/admin-topbar.component.ts
+++ client/src/app/components/admin/admin-topbar/admin-topbar.component.ts
@@ -1,20 +1,24 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AuthService } from '../../../services/auth.service';
 import { Router } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-admin-topbar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FontAwesomeModule],
   templateUrl: './admin-topbar.component.html',
   styleUrls: ['./admin-topbar.component.css']
 })
 export class AdminTopbarComponent {
-  constructor(private authService: AuthService, private router: Router) {}
+  faSignOutAlt = faSignOutAlt;
+
+  constructor(private router: Router) {}
 
   logout() {
-    this.authService.logout();
+    console.log('AdminTopbar: Logging out');
+    localStorage.removeItem('authToken'); // Remove token if used
     this.router.navigate(['/login']);
   }
 }
