908852c3906617c8d8503f627b561f60ae65adab
Justin Lee Attinoto <justinattinoto@gmail.com>
2025-05-17 15:24:16 +0200

    Refactor product card input property from 'product' to 'item' across multiple components

--- client/src/app/components/home/detaljer/detaljer.component.ts
+++ client/src/app/components/home/detaljer/detaljer.component.ts
@@ -1,6 +1,5 @@
-// src/app/components/home/detaljer/detaljer.component.ts
 import { Component, Input } from '@angular/core';
-import { CommonModule } from '@angular/common'; // Import CommonModule for ngIf, ngFor, etc.
+import { CommonModule } from '@angular/common';
 import { ProductCardComponent } from '../../common/product-card/product-card.component';
 import { Product } from '../../../models/product';
 
@@ -9,7 +8,7 @@ import { Product } from '../../../models/product';
   templateUrl: './detaljer.component.html',
   styleUrls: ['./detaljer.component.css'],
   standalone: true,
-  imports: [CommonModule, ProductCardComponent] // Add CommonModule here
+  imports: [CommonModule, ProductCardComponent]
 })
 export class DetaljerComponent {
   @Input({ required: true }) products: Product[] = [];
