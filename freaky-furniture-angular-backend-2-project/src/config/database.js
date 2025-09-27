08a93c4ba0d7b5c1852f2779f943cd106c525bab
Justin Lee Attinoto <justinattinoto@gmail.com>
2025-09-27 04:18:44 +0000

    update to p

--- freaky-furniture-angular-backend-2-project/src/config/database.js
+++ freaky-furniture-angular-backend-2-project/src/config/database.js
@@ -1,14 +1,21 @@
 const { Sequelize } = require('sequelize');
 
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_PORT:', process.env.DB_PORT);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_NAME:', process.env.DB_NAME);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+console.log('DB_SSL:', process.env.DB_SSL);
+
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: process.env.DB_HOST || 'localhost',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432,
     dialect: 'postgres',
     logging: false,
-    port: 5432,
     dialectOptions: {
       ssl: process.env.DB_SSL === 'true' ? { require: true, rejectUnauthorized: false } : false
     }
