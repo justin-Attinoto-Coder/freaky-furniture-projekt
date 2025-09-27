22a897c3d68f62d913a21ca7b3b036ac44dd3758
Justin Lee Attinoto <justinattinoto@gmail.com>
2025-09-27 12:57:02 +0200

    Remove backend project files for Freaky Furniture Angular application, including server, routes, controllers, models, middleware, and tests.

--- freaky-furniture-angular-backend-2-project/src/routes/auth.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-
-// POST /api/auth/login
-router.post('/login', authController.login);
-
-module.exports = router;
