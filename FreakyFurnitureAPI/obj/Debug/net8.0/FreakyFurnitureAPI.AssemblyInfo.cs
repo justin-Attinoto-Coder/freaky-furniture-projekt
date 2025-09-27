4b1ae14cbd4a08db2f8100d902be8effc0afea41
Justin Lee Attinoto <justinattinoto@gmail.com>
2025-09-27 14:47:10 +0200

    Update assembly info files for consistency and maintainability

--- FreakyFurnitureAPI/obj/Debug/net8.0/FreakyFurnitureAPI.AssemblyInfo.cs
+++ FreakyFurnitureAPI/obj/Debug/net8.0/FreakyFurnitureAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FreakyFurnitureAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e92adccee5f7404f8e0c507617d8bcf2ae337eaa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50d8acc1ec227df0d407b951a17932277bed458c")]
 [assembly: System.Reflection.AssemblyProductAttribute("FreakyFurnitureAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FreakyFurnitureAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
