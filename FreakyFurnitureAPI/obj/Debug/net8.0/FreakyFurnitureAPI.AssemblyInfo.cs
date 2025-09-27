50d8acc1ec227df0d407b951a17932277bed458c
Justin Lee Attinoto <justinattinoto@gmail.com>
2025-09-27 14:45:58 +0200

    updated sqlite3

--- FreakyFurnitureAPI/obj/Debug/net8.0/FreakyFurnitureAPI.AssemblyInfo.cs
+++ FreakyFurnitureAPI/obj/Debug/net8.0/FreakyFurnitureAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FreakyFurnitureAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3396b718b4f715f97bdf0cf471072b18bbb6f5a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e92adccee5f7404f8e0c507617d8bcf2ae337eaa")]
 [assembly: System.Reflection.AssemblyProductAttribute("FreakyFurnitureAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FreakyFurnitureAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
