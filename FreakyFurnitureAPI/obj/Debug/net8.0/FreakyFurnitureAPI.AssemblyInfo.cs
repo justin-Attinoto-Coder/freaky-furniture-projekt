e4c2d0d5fdffe2e5250da4809e25c973a93e2869
Justin Lee Attinoto <justinattinoto@gmail.com>
2025-09-27 15:38:27 +0200

    Update database connection string to use localhost and enable TrustServerCertificate

--- FreakyFurnitureAPI/obj/Debug/net8.0/FreakyFurnitureAPI.AssemblyInfo.cs
+++ FreakyFurnitureAPI/obj/Debug/net8.0/FreakyFurnitureAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FreakyFurnitureAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+938e85ef1b7522a246b4e52ec2b1dfe331fdc514")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92b88e410541bc7224f89c1d58bd05d81fa8e277")]
 [assembly: System.Reflection.AssemblyProductAttribute("FreakyFurnitureAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FreakyFurnitureAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
