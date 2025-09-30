5a0b2e370a85992ac540d20e196ce508f5f10a68
Justin Lee Attinoto <justinattinoto@gmail.com>
2025-09-30 16:20:10 +0200

    Refactor code structure for improved readability and maintainability

--- FreakyFurnitureAPI/obj/Debug/net8.0/FreakyFurnitureAPI.AssemblyInfo.cs
+++ FreakyFurnitureAPI/obj/Debug/net8.0/FreakyFurnitureAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FreakyFurnitureAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9461ac9e6cf7f5125cd253eb1114e40de15883d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14b302cf2ce8113dce90f54e37e1c96abd164cfe")]
 [assembly: System.Reflection.AssemblyProductAttribute("FreakyFurnitureAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FreakyFurnitureAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
