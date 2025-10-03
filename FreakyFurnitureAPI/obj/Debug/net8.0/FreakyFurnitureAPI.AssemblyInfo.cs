3c7567e5ec08109f3f9d0a9f48b0ff50926c435b
Justin Lee Attinoto <justinattinoto@gmail.com>
2025-10-03 17:42:57 +0200

    chore: Update binary files and assembly information for FreakyFurnitureAPI

--- FreakyFurnitureAPI/obj/Debug/net8.0/FreakyFurnitureAPI.AssemblyInfo.cs
+++ FreakyFurnitureAPI/obj/Debug/net8.0/FreakyFurnitureAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FreakyFurnitureAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f9517b367d81be1cf382f305873fb9511d88bcd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7e44846520ae5526592044c6c14459605dc835c")]
 [assembly: System.Reflection.AssemblyProductAttribute("FreakyFurnitureAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FreakyFurnitureAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
