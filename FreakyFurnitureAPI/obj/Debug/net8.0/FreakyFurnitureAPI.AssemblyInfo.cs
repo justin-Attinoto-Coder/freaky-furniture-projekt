fd5b75904def4f0f02160cec33c248b0442c9b6b
Justin Lee Attinoto <justinattinoto@gmail.com>
2025-10-06 20:15:44 +0200

    chore: Update binary files and remove GoToCartButtonComponent

--- FreakyFurnitureAPI/obj/Debug/net8.0/FreakyFurnitureAPI.AssemblyInfo.cs
+++ FreakyFurnitureAPI/obj/Debug/net8.0/FreakyFurnitureAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FreakyFurnitureAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb138f86927b80e49325d9457b51f215e73b399a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15c096cf4692f00e64461b3b0fc9aecd38137305")]
 [assembly: System.Reflection.AssemblyProductAttribute("FreakyFurnitureAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FreakyFurnitureAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
