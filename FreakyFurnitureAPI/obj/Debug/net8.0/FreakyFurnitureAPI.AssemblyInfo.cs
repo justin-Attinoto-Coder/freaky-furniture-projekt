ab701483bb1c8d43a20129975556b39c3dc67776
Justin Lee Attinoto <justinattinoto@gmail.com>
2025-10-07 17:58:18 +0200

    fix: Update database connection string and assembly version for production

--- FreakyFurnitureAPI/obj/Debug/net8.0/FreakyFurnitureAPI.AssemblyInfo.cs
+++ FreakyFurnitureAPI/obj/Debug/net8.0/FreakyFurnitureAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FreakyFurnitureAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73e20b4cddf6c4a097cfbdf210449227f3c7f65e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dce409b2d33dc6f5bf3d80d104feb577470d5c85")]
 [assembly: System.Reflection.AssemblyProductAttribute("FreakyFurnitureAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FreakyFurnitureAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
