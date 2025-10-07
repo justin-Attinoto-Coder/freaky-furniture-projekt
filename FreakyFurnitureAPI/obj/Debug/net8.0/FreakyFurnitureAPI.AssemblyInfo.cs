abda055646a0fc67be6cce4a47fb51f10d92900c
Justin Lee Attinoto <justinattinoto@gmail.com>
2025-10-07 08:41:13 +0200

    chore: Update assembly informational version and cache for build consistency

--- FreakyFurnitureAPI/obj/Debug/net8.0/FreakyFurnitureAPI.AssemblyInfo.cs
+++ FreakyFurnitureAPI/obj/Debug/net8.0/FreakyFurnitureAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FreakyFurnitureAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d45fe11a44879af8724c2507f11024b0833df120")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df1e843301f356436e84a7cfc42e2f94097b73da")]
 [assembly: System.Reflection.AssemblyProductAttribute("FreakyFurnitureAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FreakyFurnitureAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
